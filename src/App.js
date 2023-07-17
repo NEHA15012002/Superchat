c089e144ca4ac1bdedd5187b08dded736e94e6e1
NEHA15012002 <72308843+NEHA15012002@users.noreply.github.com>
2023-07-17 20:20:11 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react';
+import {Auth} from "./components/Auth";
+import Cookies from 'universal-cookie';
+import {Chat} from "./components/Chat";
+import {signOut} from 'firebase/auth'
+import {auth} from './Firebase'
 
-function App() {
+const cookies = new Cookies()
+
+
+
+function App(){
+  const[isAuth,setIsAuth]=useState(cookies.get("auth-token"));
+  const [room,setRoom] =useState(null);
+  const roomInputRef = React.createRef()
+  
+  const signUserOut = async ()=>{
+ 
+     await signOut(auth)
+     cookies.remove("auth-token")
+     setIsAuth(false)
+     setRoom(null)
+  }
+  
+  
+  
+  
+  if(!isAuth){
+    return(<div>
+      <Auth setIsAuth=
+      
+      {setIsAuth}/>
+      </div>);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+   <div className="firstPage">
+    
+    {room?<div><Chat room={room}/></div>:(<div className="room"><h3>Enter Room Name: <br></br></h3>
+  <input ref={roomInputRef}/>
+  <button onClick={()=>setRoom(roomInputRef.current.value)}>Enter Chat</button></div>
+  )}
+  <div className="sign-out">
+    <br></br>
+    <button onClick={signUserOut} >Sign Out</button>
+  </div>
+    
+    
     </div>
-  );
-}
+
+
+
+);}
 
 export default App;
